50d116c34da51835b27be3210464bb84828df270
sanic250 <115185839+sanic250@users.noreply.github.com>
2025-04-17 18:55:43 +0200

    final commit

--- src/app/crew/page.jsx
+++ src/app/crew/page.jsx
@@ -1,30 +1,116 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
 import styles from "./page.module.css";
 import Navbar from "../components/Navbar";
 import Image from "next/image";
-import photo from "../../../public/assets/crew/image-douglas-hurley.png";
+import douglas from "../../../public/assets/crew/image-douglas-hurley.png";
+import mark from "../../../public/assets/crew/image-mark-shuttleworth.png";
+import victor from "../../../public/assets/crew/image-victor-glover.png";
+import anousheh from "../../../public/assets/crew/image-anousheh-ansari.png";
 const Crew = () => {
+  const [value, setValue] = useState(1);
+
+  const changeValue1 = () => setValue(1);
+  const changeValue2 = () => setValue(2);
+  const changeValue3 = () => setValue(3);
+  const changeValue4 = () => setValue(4);
+
   return (
     <div className={styles.container}>
       <Navbar />
+      <div className={styles.mainTitle}>
+        <h3>
+          <span>02</span>MEET YOU CREW
+        </h3>
+      </div>
       <div className={styles.mainSection}>
-        <div className={styles.mainTitle}>
-          <span>
-            02<h3>MEET YOU CREW</h3>
-          </span>
-        </div>
         <div className={styles.middle}>
           <div className={styles.leftSide}>
-            <h3>COMMANDER</h3>
-            <h2>DOUGLAS HURLEY</h2>
-            <p>
-              Douglas Gerald Hurley is an American engineer, former Marine Corps
-              pilot and former NASA astronaut. He launched into space for the
-              third time as commander of Crew Dragon Demo-2.
-            </p>
+            {value === 1 && (
+              <div className={styles.mainText}>
+                <h3>COMMANDER</h3>
+                <h2>DOUGLAS HURLEY</h2>
+                <p>
+                  Douglas Gerald Hurley is an American engineer, former Marine
+                  Corps pilot and former NASA astronaut. He launched into space
+                  for the third time as commander of Crew Dragon Demo-2.
+                </p>
+              </div>
+            )}
+            {value === 2 && (
+              <div className={styles.mainText}>
+                <h3>MISSION SPECIALIST</h3>
+                <h2>
+                  MARK<br></br>
+                  SHUTTLEWORTH
+                </h2>
+                <p>
+                  Mark Richard Shuttleworth is the founder and CEO of Canonical,
+                  the company behind the Linux-based Ubuntu operating system.
+                  Shuttleworth became the first South African to travel to space
+                  as a space tourist.
+                </p>
+              </div>
+            )}
+            {value === 3 && (
+              <div className={styles.mainText}>
+                <h3>PILOT</h3>
+                <h2>VICTOR GLOVER</h2>
+                <p>
+                  Pilot on the first operational flight of the SpaceX Crew
+                  Dragon to the International Space Station. Glover is a
+                  commander in the U.S. Navy where he pilots an F/A-18.He was a
+                  crew member of Expedition 64, and served as a station systems
+                  flight engineer.
+                </p>
+              </div>
+            )}
+            {value === 4 && (
+              <div className={styles.mainText}>
+                <h3>FLIGHT ENGINEER</h3>
+                <h2>ANOUSHEH ANSARI</h2>
+                <p>
+                  Anousheh Ansari is an Iranian American engineer and co-founder
+                  of Prodea Systems. Ansari was the fourth self-funded space
+                  tourist, the first self-funded woman to fly to the ISS, and
+                  the first Iranian in space.
+                </p>
+              </div>
+            )}
+            <div className={styles.switch}>
+              <button
+                onClick={changeValue1}
+                className={styles.buttonSwitch}
+              ></button>
+              <button
+                onClick={changeValue2}
+                className={styles.buttonSwitch}
+              ></button>
+              <button
+                onClick={changeValue3}
+                className={styles.buttonSwitch}
+              ></button>
+              <button
+                onClick={changeValue4}
+                className={styles.buttonSwitch}
+              ></button>
+            </div>
           </div>
           <div className={styles.rightSide}>
-            <Image src={photo} alt="photo" /> 
+            {value === 1 && (
+              <Image className={styles.image} src={douglas} alt="photo" />
+            )}
+            {value === 2 && (
+              <Image className={styles.image} src={mark} alt="photo" />
+            )}
+            {value === 3 && (
+              <Image className={styles.image} src={victor} alt="photo" />
+            )}
+            {value === 4 && (
+              <Image className={styles.image} src={anousheh} alt="photo" />
+            )}
           </div>
         </div>
       </div>
--- src/app/technology/page.jsx
+++ src/app/technology/page.jsx
@@ -1,10 +1,94 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
+import Image from "next/image";
 import Navbar from "../components/Navbar";
 import styles from "./page.module.css";
+import rocket from "../../../public/assets/technology/image-launch-vehicle-portrait.jpg";
+import capsule from "../../../public/assets/technology/image-space-capsule-portrait.jpg";
+import spaceport from "../../../public/assets/technology/image-spaceport-portrait.jpg";
 const Technology = () => {
+  const [value, setValue] = useState(1);
+
+  const setValue1 = () => setValue(1);
+  const setValue2 = () => setValue(2);
+  const setValue3 = () => setValue(3);
+
   return (
     <div className={styles.container}>
       <Navbar />
+      <div className={styles.mainContainer}>
+        <div className={styles.topTitle}>
+          <h2 className={styles.titleHead}>
+            <span>03</span>
+            SPACE LAUNCH 101
+          </h2>
+        </div>
+        <div className={styles.mainSection}>
+          <div className={styles.buttons}>
+            <button onClick={setValue1} className={styles.button}>
+              1
+            </button>
+            <button onClick={setValue2} className={styles.button}>
+              2
+            </button>
+            <button onClick={setValue3} className={styles.button}>
+              3
+            </button>
+          </div>
+          {value === 1 && (
+            <div className={styles.mainText}>
+              <h3>THE TERMINOLOGY..</h3>
+              <h2>LAUNCH VEHICLE</h2>
+              <p>
+                A launch vehicle or carrier rocket is a rocket-propelled vehicle
+                used to carry a payload from Earth's surface to space, usually
+                to Earth orbit or beyond. Our WEB-X carrier rocket is the most
+                powerful in operation. Standing 150 metres tall, it's quite an
+                awe-inspiring sight on the launch pad!
+              </p>
+            </div>
+          )}
+          {value === 2 && (
+            <div className={styles.mainText}>
+              <h3>THE TERMINOLOGY..</h3>
+              <h2>SPACEPORT</h2>
+              <p>
+                A spaceport or cosmodrome is a site for launching (or receiving)
+                spacecraft, by analogy to the seaport for ships or airport for
+                aircraft. Based in the famous Cape Canaveral, our spaceport is
+                ideally situated to take advantage of the Earth’s rotation for
+                launch.
+              </p>
+            </div>
+          )}
+          {value === 3 && (
+            <div className={styles.mainText}>
+              <h3>THE TERMINOLOGY..</h3>
+              <h2>SPACE CAPSULE</h2>
+              <p>
+                A space capsule is an often-crewed spacecraft that uses a
+                blunt-body reentry capsule to reenter the Earth's atmosphere
+                without wings. Our capsule is where you'll spend your time
+                during the flight. It includes a space gym, cinema, and plenty
+                of other activities to keep you entertained.
+              </p>
+            </div>
+          )}
+          <div className={styles.imageSection}>
+            {value === 1 && (
+              <Image className={styles.image} src={rocket} alt="rocket" />
+            )}
+            {value === 2 && (
+              <Image className={styles.image} src={spaceport} alt="rocket" />
+            )}
+            {value === 3 && (
+              <Image className={styles.image} src={capsule} alt="rocket" />
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
